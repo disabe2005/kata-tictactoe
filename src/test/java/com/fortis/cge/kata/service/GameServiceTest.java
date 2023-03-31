871e006a808cf43c9792c8c2d5e1e6f6aaae9743
disabe2005 <didiersabecheun@gmail.com>
2023-03-31 11:40:56 +0200

    update Test

--- src/test/java/com/fortis/cge/kata/service/GameServiceTest.java
+++ src/test/java/com/fortis/cge/kata/service/GameServiceTest.java
@@ -10,7 +10,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.fortis.cge.kata.model.GameDraw;
